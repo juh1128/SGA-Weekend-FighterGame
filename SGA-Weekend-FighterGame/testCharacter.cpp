ba8b0110aab8b9ac98f9c2f581c9647b7c4b939a
정유훈 <juh17@naver.com>
2017-09-18 02:31:17 +0900

    테스트 캐릭터, 솔배드가이 버그 수정

--- SGA-Weekend-FighterGame/testCharacter.cpp
+++ SGA-Weekend-FighterGame/testCharacter.cpp
@@ -8,8 +8,8 @@ HRESULT testCharacter::init(vector2D pos)
 	setupResource();
 
 	//테스트 애니메이션은 setupYuhoon에서 만든다. (게임 시작 시 최초 1번만 만들어놓고 씀)
-	character::init("테스트 캐릭터", pos, "테스트_공격_오른쪽");
-	this->changeState(testCharacterState::RIGHT_ATTACK);
+	character::init("테스트 캐릭터", pos, "테스트_이동_오른쪽");
+	this->changeState(testCharacterState::RIGHT_MOVE);
 
 	//this->setScale(4.0f, 4.0f);
 	//this->setSize(150, 300);
