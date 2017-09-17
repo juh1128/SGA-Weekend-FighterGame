55b6ce142a9c41f7019af86890b8026c514c1ed5
정유훈 <juh17@naver.com>
2017-09-17 18:14:31 +0900

    영재 똥

--- SGA-Weekend-FighterGame/setupYoungjae.cpp
+++ SGA-Weekend-FighterGame/setupYoungjae.cpp
@@ -4,7 +4,6 @@
 void playGround::setupYoungjae()
 {
 	//이미지 리소스 로드
-	IMAGEMANAGER->addFrameImage("맵", "resource/background2.bmp", 1828, 6144, 1, 8, true);
 	IMAGEMANAGER->addFrameImage("Nanaya_Right_Stand", "resource/youngjae/Nanaya/R_Stand.bmp", 4096, 256, 16, 1, true);
 
 	//애니메이션 로드
