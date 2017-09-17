181a424c7e0dabbb2fda5cec08c2716b5efa7d24
정성훈 <kmjung1515@naver.com>
2017-09-17 17:36:11 +0900

    마이 cpp와h 추가 셋업 업데이트

---
+++ SGA-Weekend-FighterGame/mai.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "mai.h"
+
+HRESULT mai::init(vector2D pos)
+{
+
+	return S_OK;
+}
+
+void mai::release()	
+{
+
+}
+void mai::update()	
+{
+
+}
+void mai::render()	
+{
+
+}
+
+void mai::stateUpdate(state) 
+{
+
+}
+void mai::changeState(state) 
+{
+
+}
