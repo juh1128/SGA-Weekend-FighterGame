b2a5e3161001329e558ff56ecb3fd761b902aefb
순우 <paper3300@naver.com>
2017-09-18 17:51:01 +0900

    neko관련

--- SGA-Weekend-FighterGame/neko.h
+++ SGA-Weekend-FighterGame/neko.h
@@ -11,25 +11,26 @@ namespace tagNekoState
 {
 	enum ENUM
 	{
-		RIGHT_STOP, LEFT_STOP,				//기본상태
-		RIGHT_MOVE, LEFT_MOVE,				//이동상태
-		RIGHT_JUMP, LEFT_JUMP,				//점프
+		RIGHT_STOP, LEFT_STOP,					//기본상태
+		RIGHT_MOVE, LEFT_MOVE,					//이동상태
+		RIGHT_JUMP, LEFT_JUMP,					//점프
 		RIGHT_BACK_JUMP, LEFT_BACK_JUMP,		//백점프
-		RIGHT_SIT, LEFT_SIT,				//앉기
-		RIGHT_CHANGE_SIT, LEFT_CHANGE_SIT,	//앉는 모션
-		RIGHT_CHANGE_UP, LEFT_CHAGNE_UP,	//일어서는 모션
-		RIGHT_SIT_MOVE, LEFT_SIT_MOVE,		//앉아서 이동
-		RIGHT_ATTACK, LEFT_ATTACK,			//약공	
-		RIGHT_SIT_ATTACK, LEFT_SIT_ATTACK,	//앉아서 약공
-		RIGHT_DEFENSE, LEFT_DEFENSE,		//막기
+		RIGHT_SIT, LEFT_SIT,					//앉기
+		RIGHT_CHANGE_SIT, LEFT_CHANGE_SIT,		//앉는 모션
+		RIGHT_CHANGE_UP, LEFT_CHAGNE_UP,		//일어서는 모션
+		RIGHT_SIT_MOVE, LEFT_SIT_MOVE,			//앉아서 이동
+		RIGHT_ATTACK, LEFT_ATTACK,				//약공	
+		RIGHT_SIT_ATTACK, LEFT_SIT_ATTACK,		//앉아서 약공
+		RIGHT_DEFENSE, LEFT_DEFENSE,			//막기
 		RIGHT_SIT_DEFENSE, LEFT_SIT_DEFENSE,	//앉아 막기
-		RIGHT_KICK, LEFT_KICK,				//발차기
-		RIGHT_SIT_KICK, LEFT_SIT_KICK,		//앉아 발차기
-		RIGHT_FIRE, LEFT_FIRE,				//불뿜기
-		RIGHT_STAY_FIRE, LEFT_STAY_FIRE,	//불뿜고 있기
-		RIGHT_FLY, LEFT_FLY,				//날기
-		RIGHT_FALL,LEFT_FALL
-
+		RIGHT_KICK, LEFT_KICK,					//발차기
+		RIGHT_SIT_KICK, LEFT_SIT_KICK,			//앉아 발차기
+		RIGHT_FIRE, LEFT_FIRE,					//불뿜기
+		RIGHT_STAY_FIRE, LEFT_STAY_FIRE,		//불뿜고 있기
+		RIGHT_FLY, LEFT_FLY,					//날기
+		RIGHT_FALL,LEFT_FALL,					//떨어지는 상태
+		RIGHT_DASH,LEFT_DASH,					//대쉬 상태
+		RIGHT_BACK_DASH , LEFT_BACK_DASH		//백대쉬 상태
 		
 	};
 }
@@ -82,9 +83,14 @@ public:
 		_jumpPower = NEKOJUMPPOWER;
 	}
 
-	void nekoFire();
 
+	//커맨드 함수들 
+	void nekoFire();
 	void nekoRightFly();
 	void nekoLeftFly();
+	void nekoRightDash();
+	void nekoLeftDash();
+	void nekoRightBackDash();
+	void nekoLeftBackDash();
 };
 
